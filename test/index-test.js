28fb0e51cbbee48a1f7b42dc25d946878ba9f2ac
Daban <dabansadiq@icloud.com>
2021-11-01 22:41:35 +0300

    test

--- test/index-test.js
+++ test/index-test.js
@@ -30,36 +30,36 @@ describe('App', () => {
   });
 });
 
-// describe('InFrontOfYou', () => {
-//   let wrapper;
-//
-//   before(() => {
-//     wrapper = shallow(React.createElement(InFrontOfYou));
-//   });
-//
-//   it('renders withouth crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<InFrontOfYou />, div);
-//   });
-//
-//   it('should have the right DOM markup', () => {
-//     expect(wrapper.html()).to.equal(`<div><p>You shouldn&#x27;t look too far.</p><p>Sometimes, the solution is right in front of you.</p></div>`);
-//   });
-// });
-//
-// describe('ButcherShop', () => {
-//   let wrapper;
-//
-//   before(() => {
-//     wrapper = shallow(React.createElement(ButcherShop));
-//   });
-//
-//   it('renders withouth crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<ButcherShop />, div);
-//   });
-//
-//   it('should have the right DOM markup', () => {
-//     expect(wrapper.html()).to.equal(`<div class="butcher-shop"><p>Hello! We have the following products for sale today:</p><ul><li>Tenderloin</li><li>Short ribs</li><li>Beef shin</li><li>Ribeye</li></ul></div>`);
-//   });
-// });
+describe('InFrontOfYou', () => {
+  let wrapper;
+
+  before(() => {
+    wrapper = shallow(React.createElement(InFrontOfYou));
+  });
+
+  it('renders withouth crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<InFrontOfYou />, div);
+  });
+
+  it('should have the right DOM markup', () => {
+    expect(wrapper.html()).to.equal(`<div><p>You shouldn&#x27;t look too far.</p><p>Sometimes, the solution is right in front of you.</p></div>`);
+  });
+});
+
+describe('ButcherShop', () => {
+  let wrapper;
+
+  before(() => {
+    wrapper = shallow(React.createElement(ButcherShop));
+  });
+
+  it('renders withouth crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ButcherShop />, div);
+  });
+
+  it('should have the right DOM markup', () => {
+    expect(wrapper.html()).to.equal(`<div class="butcher-shop"><p>Hello! We have the following products for sale today:</p><ul><li>Tenderloin</li><li>Short ribs</li><li>Beef shin</li><li>Ribeye</li></ul></div>`);
+  });
+});
